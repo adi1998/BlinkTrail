06985e197dcb5738ddc98ae2d2f95d2c8c0e3d9c
Aditya Gupta <adi1998.2010@gmail.com>
2026-04-22 17:40:55 +0530

    cleanup obstacles in hetia and demeter boons

--- src/gods/demeter_reload.lua
+++ src/gods/demeter_reload.lua
@@ -14,46 +14,28 @@ end
 
 function mod.StartDemeterBlink( args )
     game.MapState[_PLUGIN.guid .. "DemeterTurretMap"] = game.MapState[_PLUGIN.guid .. "DemeterTurretMap"] or {}
-    if not game.IsEmpty(game.MapState.BlinkDropTrail) then
-        for id, ids in pairs(game.MapState.BlinkDropTrail) do
-            -- game.SetAnimation({ Name = "ProjectileLightningBallEnd", DestinationId = id , DataProperties = {Duration = 0.2}})
-            game.thread(game.DestroyOnDelay, { id }, 0.1 )
-        end
-        game.MapState.BlinkDropTrail = {}
-    end
     local initialId = game.SpawnObstacle({ Name = "BlankObstacle", DestinationId = game.CurrentRun.Hero.ObjectId, Group = "Standing" })
     -- local angle = game.GetAngle({ Id = game.CurrentRun.Hero.ObjectId })
-    local prevProj = game.SpawnObstacle({ Name = "BlankObstacle", DestinationId = game.CurrentRun.Hero.ObjectId, Group = "Standing" })
     local blinkIds = { initialId }
     local nextClipRegenTime  = game.GetWeaponDataValue({ Id = game.CurrentRun.Hero.ObjectId, WeaponName = "WeaponBlink", Property = "ClipRegenInterval" }) or 0
     local waitPeriod = nextClipRegenTime + (game.GetWeaponDataValue({ Id = game.CurrentRun.Hero.ObjectId, WeaponName = "WeaponBlink", Property = "BlinkDuration" }) or 0) - 0.1
     local startTime = game._worldTime
-    local maxTrailLength = 99
     game.MapState.BlinkDropTrail = game.MapState.BlinkDropTrail or {}
     game.MapState.BlinkDropTrail[initialId] = blinkIds
     local fx_index = 5
-    local delay_count = 0
-    local anim_list = {}
     local skippedLast = false
     while game.MapState.BlinkDropTrail and game.MapState.BlinkDropTrail[initialId] and (game._worldTime - startTime) < waitPeriod and fx_index >= 0 do
         game.wait(0.2, "BlinkTrailPresentation")
         local distance = game.GetDistance({ Id = blinkIds [#blinkIds], DestinationId = game.CurrentRun.Hero.ObjectId })
-        -- print("distance", distance)
         if distance > 140 or distance > 40 and skippedLast then
             local targetId = game.SpawnObstacle({ Name = "BlankObstacle", DestinationId = game.CurrentRun.Hero.ObjectId, Group = "Standing" })
-            local targetProjId = game.SpawnObstacle({ Name = "BlankObstacle", DestinationId = game.CurrentRun.Hero.ObjectId, Group = "Standing" })
             local angle = game.GetAngleBetween({ DestinationId = targetId, Id = blinkIds [#blinkIds] })
             table.insert( blinkIds, targetId )
             game.SetAnimation({ Name = "BlinkTrailDemeterTurret", DestinationId = blinkIds [#blinkIds - 1]})
             game.thread(game.DestroyOnDelay, { blinkIds [#blinkIds - 1] }, 3.4 )
-            -- game.CreateAnimationsBetween({
-            --     Animation = "BlinkGhostTrail_DemeterFx", DestinationId = blinkIds [#blinkIds], Id = blinkIds [#blinkIds - 1],
-            --     Stretch = true, UseZLocation = false})
             game.thread(mod.DemeterProjectileWithDelay,
-                { Name = args.ProjectileName, Id = game.CurrentRun.Hero.ObjectId, Angle = (180 + angle) % 360, DamageMultiplier = args.DamageMultiplier, FireFromId = targetProjId, DataProperties = {Range = distance, MaxAdjustRate = 0, AttachToOwner = false}, FizzleOldestProjectileCount = 6 }
+                { Name = args.ProjectileName, Id = game.CurrentRun.Hero.ObjectId, Angle = (180 + angle) % 360, DamageMultiplier = args.DamageMultiplier, FireFromId = targetId, DataProperties = {Range = distance, MaxAdjustRate = 0, AttachToOwner = false}, FizzleOldestProjectileCount = 6 }
             , 0.4, blinkIds[#blinkIds - 1] )
-            prevProj = targetProjId
-            -- angle = game.GetAngle({ Id = game.CurrentRun.Hero.ObjectId })
             skippedLast = false
         else
             skippedLast = true
@@ -62,48 +44,15 @@ function mod.StartDemeterBlink( args )
     game.wait(0.25, "BlinkTrailPresentation")
     game.SetAnimation({ Name = "BlinkTrailDemeterTurret", DestinationId = blinkIds [#blinkIds]})
     game.thread(game.DestroyOnDelay, { blinkIds [#blinkIds] }, 3.4 )
-    local angle = game.GetAngleBetween({ DestinationId = blinkIds [#blinkIds], Id = blinkIds [#blinkIds-1] })
-    local distance = game.GetDistance({ Id = blinkIds [#blinkIds], DestinationId = blinkIds [#blinkIds-1] })
+
     local unitId = game.SpawnUnit({ Name = "DummyOlympusTarget", Group = "Standing", DestinationId = blinkIds[#blinkIds], DataProperties = {CollideWithUnits = false} })
     game.thread(game.DestroyOnDelay, { unitId }, 3.4 )
     game.SetUnitProperty({ DestinationId = unitId, Property = "CollideWithUnits", Value = false })
     game.thread(mod.DemeterProjectileWithDelay,
         { Name = args.ProjectileName, Id = game.CurrentRun.Hero.ObjectId, DestinationId = unitId, DamageMultiplier = args.DamageMultiplier, FizzleOldestProjectileCount = 6 }
     , 0.4, blinkIds[#blinkIds])
-    -- game.thread(mod.DemeterProjectileWithDelay,
-    --     { Name = "FamiliarLinkLaser", Id = game.CurrentRun.Hero.ObjectId, DestinationId = unitId, DamageMultiplier = args.DamageMultiplier,  }
-    -- , 0.4)
+
     if game.MapState.BlinkDropTrail then
         game.MapState.BlinkDropTrail[ initialId ] = nil
     end
-    -- print("blink id count", #blinkIds)
-    local lastItemId = table.remove( blinkIds )
-    -- game.Destroy({Id = lastItemId})
-    local outDuration = 0.16 -- time to remove trail over
-    local waitInterval = outDuration/#blinkIds
-    local minWaitInterval = 0.06
-    local skipInterval = 1
-    local skipCounter = 0
-    if waitInterval < minWaitInterval then
-        local multiplier = math.ceil(minWaitInterval/waitInterval)
-        waitInterval = waitInterval * multiplier
-        skipInterval = multiplier
-    end
-
-    local finalAnchor = game.SpawnObstacle({ Name = "BlankObstacle", DestinationId = game.CurrentRun.Hero.ObjectId, Group = "Standing" })
-    game.Attach({ Id = finalAnchor, DestinationId = game.CurrentRun.Hero.ObjectId })
-    if game.GetDistance({ Id = finalAnchor, DestinationId = game.CurrentRun.Hero.ObjectId }) > 0 then
-        -- game.CreateAnimationsBetween({ Animation = "BlinkLightningBall", DestinationId = blinkIds [#blinkIds - 1], Id = finalAnchor, Stretch = false, UseZLocation = false})
-    end
-    while not game.IsEmpty( blinkIds ) do
-        while skipCounter < skipInterval do
-            local lastItemId = table.remove( blinkIds, 1 )
-            -- game.SetAnimation({ Name = "ProjectileLightningBallEnd", DestinationId = lastItemId, DataProperties = {Duration = 0.2} })
-            -- game.thread(DestroyOnDelay, { lastItemId }, 0.1 )
-            skipCounter = skipCounter + 1
-        end
-        skipCounter = 0
-        game.wait( waitInterval, "BlinkTrailPresentation")
-    end
-    -- Destroy({ Id = finalAnchor })
 end
